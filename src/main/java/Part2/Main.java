09cd7ada3843976209e6b10115ed1cad2fe631a4
SabreT00th84 <91007752+SabreT00th84@users.noreply.github.com>
2026-04-26 21:36:56 +0100

    refactor: Wrap launch with startRaceGUI() to meet requirements

--- src/main/java/Part2/Main.java
+++ src/main/java/Part2/Main.java
@@ -9,7 +9,11 @@ import javafx.stage.Stage;
 public class Main extends Application {
 
     public static void main(String[] args) {
-        launch(args);
+        startRaceGUI();
+    }
+
+    public static void startRaceGUI() {
+        launch();
     }
 
     @Override
